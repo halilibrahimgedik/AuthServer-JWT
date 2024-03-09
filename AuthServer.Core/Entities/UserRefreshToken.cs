a5c3ae4716ddeebb04ff1ef6ecb7bb365be86111
Halil Gedik <halilgedik4234@gmail.com>
2024-03-09 21:16:55 +0300

    Added IGenericRepository interface

--- AuthServer.Core/Entities/UserRefreshToken.cs
+++ AuthServer.Core/Entities/UserRefreshToken.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace AuthServer.Core.Entities
 {
-    internal class UserRefreshToken
+    public class UserRefreshToken
     {
+        public string UserId { get; set; }
+        public string RefreshToken { get; set; }
+
+        public DateTime Expiration { get; set; }
     }
 }
---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetAsync(int id);
+
+        IQueryable<TEntity> GetAllAsync();
+
+        IQueryable<TEntity> Where(Expression<Func<TEntity,bool>> predicate);
+
+        Task AddAsync(TEntity entity); // memory'e bir data eklediğinden dolayı bir asenkron metodu vardır. daha sonra SaveChanges methodu çağırılarak memorydeki entityler kontrol edilir ve veri tabanına kaydedilir.
+        void Remove(int id); // entity'nin state'ini 'deleted' olarak işaretler bu yüzden asenkron metodu yok
+
+        TEntity Update(TEntity entity);
+    }
+}
