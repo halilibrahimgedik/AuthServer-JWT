5ace5cd8b9b84d7791bd9bd3bcc45e1e8f5d5a6d
Halil Gedik <halilgedik4234@gmail.com>
2024-03-09 23:18:40 +0300

    Added IServiceGeneric class and Added a class that does not return data

---
+++ AuthServer.Core/Service/IServiceGeneric.cs
@@ -0,0 +1,24 @@
+﻿using SharedLibrary.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Service
+{
+    public interface IServiceGeneric<TEntity, TDto> where TEntity : class where TDto : class
+    {
+        Task<Response<TDto>> GetByIdAsync(int id); // geriye dto nesneleri dönelim
+
+        Task<Response<IEnumerable<TDto>>> GetAllAsync();
+
+        Response<IEnumerable<TDto>> Where(Expression<Func<TEntity, bool>> predicate); //where, (product=>product.ıd>5) gibi
+
+        Task<Response<TDto>> AddAsync(TEntity entity); // Db'de oluşturduğumuz entity'yi dto olarak geri dönderelim
+        Task<Response<NoDataDto>> Remove(int id); // artık void döndüremeyiz, bu yüzden geriye boş nesne gönderelim
+
+        Task<Response<NoDataDto>> Update(TEntity entity);
+    }
+}
