1b5c24d2a71f0cfd2809bee12ef0720373836b32
Halil Gedik <halilgedik4234@gmail.com>
2024-03-09 23:37:56 +0300

    added ITokenService , Client.cs

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+
+        public string Secret { get; set; }
+
+        // Client'ın erişebileceği api servisleri listesi
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ AuthServer.Core/Service/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Service
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
