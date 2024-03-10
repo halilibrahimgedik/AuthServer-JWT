47dbd89622bac3bddd0f7f5074f2d39e76e981c5
Halil Gedik <halilgedik4234@gmail.com>
2024-03-10 23:07:49 +0300

    Added Automapper Library

---
+++ AuthServer.Core/Dtos/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+
+        //public int Stock { get; set; }
+        public string UserId { get; set; }
+    }
+}
