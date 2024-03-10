d499551a1394efb515265cdf43711a610ac91363
Halil Gedik <halilgedik4234@gmail.com>
2024-03-10 14:06:30 +0300

    Created UnitOfWork.cs

---
+++ AuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using AuthServer.Core.IUnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
