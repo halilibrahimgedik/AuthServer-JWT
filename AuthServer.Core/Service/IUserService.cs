1a0e83a09bbdbe4b1c994c902b78cc209e53fd60
Halil Gedik <halilgedik4234@gmail.com>
2024-03-10 12:30:11 +0300

    Added IUserService interface

---
+++ AuthServer.Core/Service/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using AuthServer.Core.Dtos;
+using SharedLibrary.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Service
+{
+    public interface IUserService
+    {
+        Task<Response<CreateUserDto>>
+    }
+}
