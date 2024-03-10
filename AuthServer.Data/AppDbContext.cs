b570d2d998d57e8bdd28f1002ab08ab968b5aa3f
Halil Gedik <halilgedik4234@gmail.com>
2024-03-10 12:45:32 +0300

    Created DbContext class

---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AuthServer.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    public class AppDbContext : IdentityDbContext<UserApp,IdentityRole,string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+
+    }
+}
