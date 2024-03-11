948f6e69427b9a507c7e9482cecec2d11083db34
Halil Gedik <halilgedik4234@gmail.com>
2024-03-11 16:26:37 +0300

    Added SignService class

---
+++ AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public static class SignService
+    {
+        public static SecurityKey GetSymetricSecurityKey(string securityKey)
+        {
+
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
