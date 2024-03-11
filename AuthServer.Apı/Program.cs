4565386d85fc3256054a122e47f8b689159128c2
Halil Gedik <halilgedik4234@gmail.com>
2024-03-11 22:40:49 +0300

    Added Option Pattern For Client

--- AuthServer.Apı/Program.cs
+++ AuthServer.Apı/Program.cs
@@ -1,3 +1,4 @@
+using AuthServer.Core.Configuration;
 using SharedLibrary.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+builder.Services.Configure<Client>(builder.Configuration.GetSection("Clients"));
 
 var app = builder.Build();
 
