76b513ae248b099ef735b00a2e34ee55738753f4
Halil Gedik <halilgedik4234@gmail.com>
2024-03-15 10:26:15 +0300

    Added Auth controller

---
+++ AuthServer.Apı/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Apı.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken(LoginDto loginDto)
+        {
+            var result = await _authenticationService.CreateTokenAsync(loginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public  IActionResult CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            var result = _authenticationService.CreateTokenByClient(clientLoginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+
+        {
+            var result = await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.RefreshToken);
+
+            return ActionResultInstance(result);
+        }
+    }
+}
