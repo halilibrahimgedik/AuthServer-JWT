1d6b592a8cbb1de569bc8ae3b74bab11887a066b
Halil Gedik <halilgedik4234@gmail.com>
2024-05-15 11:55:33 +0300

    activated app.UseAuthentication() middleware in Api projects

--- MiniApp1.API/Program.cs
+++ MiniApp1.API/Program.cs
@@ -38,6 +38,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
--- MiniApp3.API/Program.cs
+++ MiniApp3.API/Program.cs
@@ -40,6 +40,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
